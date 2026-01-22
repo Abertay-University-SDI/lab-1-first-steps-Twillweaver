fa410948872f28f46b6efb6890e123391ed7a9f2
Kristina M <a.tea.moth@gmail.com>
2026-01-22 12:01:12 +0000

    Implemented snake movement, sustained direction, and window-bound reset

--- Coursework/CMP105App/Level.cpp
+++ Coursework/CMP105App/Level.cpp
@@ -5,6 +5,10 @@ Level::Level(sf::RenderWindow& hwnd, Input& in) :
 {
 	// initialise game objects
 
+	m_player.setRadius(10.f);
+	m_player.setFillColor(sf::Color::Green);
+	m_player.setPosition({ 300.f, 300.f });
+
 }
 
 // handle user input
@@ -15,17 +19,72 @@ void Level::handleInput(float dt)
 		std::cout << "left mouse pressed" << std::endl;
 	}
 
+	if (m_input.isRightMousePressed())
+	{
+		std::cout << "right mouse pressed" << std::endl;
+	}
+
+	if (m_input.isKeyDown(sf::Keyboard::Scancode::D))
+		m_direction = Direction::RIGHT;
+
+	if (m_input.isKeyDown(sf::Keyboard::Scancode::A))
+		m_direction = Direction::LEFT;
+
+	if (m_input.isKeyDown(sf::Keyboard::Scancode::W))
+		m_direction = Direction::UP;
+
+	if (m_input.isKeyDown(sf::Keyboard::Scancode::S))
+		m_direction = Direction::DOWN;
+
 }
 
 // Update game objects
 void Level::update(float dt)
 {
+	// sustained movement
+	switch (m_direction)
+	{
+	case Direction::UP:
+		m_player.move({ 0.f, -m_speed * dt });
+		break;
+
+	case Direction::DOWN:
+		m_player.move({ 0.f, m_speed * dt });
+		break;
+
+	case Direction::LEFT:
+		m_player.move({ -m_speed * dt, 0.f });
+		break;
+
+	case Direction::RIGHT:
+		m_player.move({ m_speed * dt, 0.f });
+		break;
+	}
+
+	// Boundary check
+	sf::Vector2f pos = m_player.getPosition();
+	float radius = m_player.getRadius();
+	sf::Vector2u windowSize = m_window.getSize();
+
+	// Check X bounds
+	if (pos.x > windowSize.x - radius * 2 || pos.x < 0)
+	{
+		m_player.setPosition({ windowSize.x * 0.5f, windowSize.y * 0.5f });
+	}
+
+	// Check Y bounds
+	if (pos.y > windowSize.y - radius * 2 || pos.y < 0)
+	{
+		m_player.setPosition({ windowSize.x * 0.5f, windowSize.y * 0.5f });
+	}
 }
 
 // Render level
 void Level::render()
 {
 	beginDraw();
+
+	m_window.draw(m_player);
 
 	endDraw();
 }
--- Coursework/CMP105App/Level.h
+++ Coursework/CMP105App/Level.h
@@ -1,18 +1,29 @@
 #pragma once
 
 #include "Framework/BaseLevel.h"
+#include <SFML/Graphics.hpp>
 
-class Level : BaseLevel{
+class Level : public BaseLevel{
 public:
 	Level(sf::RenderWindow& window, Input& input);
 	~Level() override = default;
 
 	void handleInput(float dt) override;
-	void update(float dt);
-	void render();
+	void update(float dt) override;
+	void render() override;
 
 private:
 	// Default functions for rendering to the screen.
+
+	// player representation
+	sf::CircleShape m_player;
+
+	// player speed
+	float m_speed = 200.f;
+
+	// movement direction
+	enum class Direction { UP, DOWN, LEFT, RIGHT };
+	Direction m_direction = Direction::RIGHT;
 
 	// Default variables for level class.
 	
